2e90003f4d3fa1b8b1e859efed453fe373adfeac
lijie <lijie@ruizhiqi.com>
2018-07-05 09:28:56 +0800

    我是呵呵

--- git_day1/src/main/java/Test.java
+++ git_day1/src/main/java/Test.java
@@ -2,6 +2,10 @@ public class Test {
 
     //绿色为暂存区
     public static void main(String[] args) {
+
         System.out.println("  wo shi  li jie");
+
+
+        System.out.println("  wo  shi  hehe");
     }
 }
